46f5fa415b21cd198b552036a168c8b4390e4068
Muhammad Kevin <muhammad.kevin@prudential.co.id>
2022-01-01 22:59:13 +0700

    update wording for timeline

--- src/module/Timeline.jsx
+++ src/module/Timeline.jsx
@@ -1,98 +1,62 @@
 import React from 'react';
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
+import { VerticalTimeline } from 'react-vertical-timeline-component';
 import { Container } from 'react-bootstrap';
-import { BsFillHeartFill } from 'react-icons/bs';
 import 'react-vertical-timeline-component/style.min.css';
-import { first_date, engagement, wedding } from '../assets'
+import { first_date, engagement, wedding } from '../assets';
+import TimelineItem from '../components/TimelineItem';
 
 function Timeline() {
+  const timelineData = [
+    {
+      title: 'How We Meet',
+      image:
+        'https://www.osceolaschools.net/cms/lib/FL50000609/Centricity/Shared/Images/EagerStudents.jpg',
+      date: 'Bandung, 2006',
+      description:
+        'Our first met was in elementary school. At that time, Kevin was a transfer student in the 3th grade of elementary school and he sat at the same table with Shalma. From there, we often joked and liked each other like elementary school children.',
+    },
+    {
+      title: 'How We Meet (Again)',
+      image: first_date,
+      date: 'Bandung, 2016',
+      description:
+        'After graduating from elementary school on 2009, we lost contact for almost 7 years and we started to meet again on the elementary school reunion. Long story short, it turns out that Kevin still likes Shalma. From that day, Kevin ask her to go on a date.',
+    },
+    {
+      title: 'First Date',
+      image: first_date,
+      date: 'Bandung, 2016',
+      description:
+        "Our first date was going to mall, we spend our time chit-chat like 'a lost buddy' that never seen in a long time. We flashback on our Elementary School life, and we ask each other why we never see each other even our High School is on the same neighborhood. Maybe it's some God plans that we met again after 7 years. lol",
+    },
+    {
+      title: 'Engagement',
+      image: engagement,
+      date: 'Bandung, 2021',
+      description:
+        "It's been almost 5 years since we've been in a relationship and we started planning for a more serious relationship. Finally, we decided to engaged on May 29, 2021.",
+    },
+    {
+      title: 'Wedding Day',
+      image: wedding,
+      date: 'Bandung, 2022',
+      description:
+        'We never thought that it started from elementary school friends and ended up on the aisle to become friends for life on February 20, 2022.',
+    },
+  ];
+
   return (
     <Container className="my-5">
       <h2 className="font-playfair text-center">Our Story</h2>
       <VerticalTimeline>
-        <VerticalTimelineElement
-          iconStyle={{ background: 'rgb(230, 175, 83)', color: '#fff' }}
-          className="font-playfair"
-          icon={<BsFillHeartFill />}
-        >
-          <img
-            src="https://www.osceolaschools.net/cms/lib/FL50000609/Centricity/Shared/Images/EagerStudents.jpg"
-            alt="student"
-            className="photo-timeline mb-4"
+        {timelineData.map((item, index) => (
+          <TimelineItem
+            image={item.image}
+            title={item.title}
+            date={item.date}
+            description={item.description}
           />
-          <h3 className="vertical-timeline-element-title fw-bold ">
-            How We Meet
-          </h3>
-          <h5 className="vertical-timeline-element-subtitle ">Bandung, 2006</h5>
-          <p>We are on a same elementary school</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="font-playfair"
-          iconStyle={{ background: 'rgb(230, 175, 83)', color: '#fff' }}
-          icon={<BsFillHeartFill />}
-        >
-          <img
-            src={first_date}
-            alt="First Date"
-            className="photo-timeline mb-4"
-          />
-          <h3 className="vertical-timeline-element-title fw-bold ">
-            How We Meet (Again)
-          </h3>
-          <h5 className="vertical-timeline-element-subtitle ">Bandung, 2016</h5>
-          <p>We meet again on Elementary School Reunion</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="font-playfair"
-          iconStyle={{ background: 'rgb(230, 175, 83)', color: '#fff' }}
-          icon={<BsFillHeartFill />}
-        >
-          <img
-            src={first_date}
-            alt="First Date"
-            className="photo-timeline mb-4"
-          />
-          <h3 className="vertical-timeline-element-title fw-bold ">
-            First Date
-          </h3>
-          <h5 className="vertical-timeline-element-subtitle ">Bandung, 2016</h5>
-          <p>We are on a same elementary school</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="font-playfair"
-          iconStyle={{ background: 'rgb(230, 175, 83)', color: '#fff' }}
-          icon={<BsFillHeartFill />}
-        >
-          <img
-            src={engagement}
-            alt="First Date"
-            className="photo-timeline mb-4"
-          />
-          <h3 className="vertical-timeline-element-title fw-bold ">
-            Engagement
-          </h3>
-          <h5 className="vertical-timeline-element-subtitle ">Bandung, 2021</h5>
-          <p>We are on a same elementary school</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="font-playfair"
-          iconStyle={{ background: 'rgb(230, 175, 83)', color: '#fff' }}
-          icon={<BsFillHeartFill />}
-        >
-          <img
-            src={wedding}
-            alt="First Date"
-            className="photo-timeline mb-4"
-          />
-          <h3 className="vertical-timeline-element-title fw-bold ">
-            Wedding Day
-          </h3>
-          <h5 className="vertical-timeline-element-subtitle ">Bandung, 2022</h5>
-          <p>We are on a same elementary school</p>
-        </VerticalTimelineElement>
+        ))}
       </VerticalTimeline>
     </Container>
   );
